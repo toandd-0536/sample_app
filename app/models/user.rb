33d09468165aa6c890aab13efb4838c529e3f4bf
toandd-0536 <doan.duc.toan@sun-asterisk.com>
2024-07-26 10:54:54 +0700

    [Naitei] chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,16 +9,38 @@ class User < ApplicationRecord
             presence: true,
             length: {maximum: Settings.models.user.email.max_length},
             format: {with: Settings.models.user.email.valid_email_regex}
+  attr_accessor :remember_token
 
-  def self.digest string
-    cost = if ActiveModel::SecurePassword.min_cost
-             BCrypt::Engine::MIN_COST
-           else
-             BCrypt::Engine.cost
-           end
-    BCrypt::Password.create(string, cost:)
+  class << self
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create(string, cost:)
+    end
   end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_column :remember_digest, nil
+  end
+
   private
+
   def downcase_email
     email.downcase!
   end
