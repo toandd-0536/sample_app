58e43ad7fda542a03577587ab9663eb002380999
toandd-0536 <doan.duc.toan@sun-asterisk.com>
2024-07-29 18:27:25 +0700

    [naitei] Chapter 11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params.dig(:session, :email)&.downcase
     if user.try :authenticate, params.dig(:session, :password)
-      log_in user
-      params.dig(:session, :remember_me) == "1" ? remember(user) : forget(user)
-      flash[:success] = t("controllers.sessions.login_message")
-      redirect_back_or user
+      if user.activated?
+        forwarding_url = session[:forwarding_url]
+        reset_session
+        params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+        log_in user
+        redirect_to forwarding_url || user
+      else
+        flash[:warning] = t "controllers.sessions.warning"
+        redirect_to root_url, status: :see_other
+      end
     else
       flash.now[:danger] = t("controllers.sessions.danger")
       render :new, status: :unprocessable_entity
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   before_save :downcase_email
-
+  before_create :create_activation_digest
   validates :name,
             presence: true,
             length: {maximum: Settings.models.user.name.max_length}
@@ -9,8 +9,11 @@ class User < ApplicationRecord
             presence: true,
             length: {maximum: Settings.models.user.email.max_length},
             format: {with: Settings.models.user.email.valid_email_regex}
-  attr_accessor :remember_token
+
+  attr_accessor :remember_token, :activation_token
+
   scope :ordered, ->{order :created_at}
+
   class << self
     def new_token
       SecureRandom.urlsafe_base64
@@ -22,7 +25,7 @@ class User < ApplicationRecord
              else
                BCrypt::Engine.cost
              end
-      BCrypt::Password.create(string, cost:)
+      BCrypt::Password.create string, cost:
     end
   end
 
@@ -31,17 +34,33 @@ class User < ApplicationRecord
     update_column :remember_digest, User.digest(remember_token)
   end
 
-  def authenticated? remember_token
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated? attribute, token
+    digest = public_send "#{attribute}_digest"
+    return false unless digest
+
+    BCrypt::Password.new(digest).is_password? token
   end
 
   def forget
     update_column :remember_digest, nil
   end
 
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
 
   def downcase_email
     email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest activation_token
   end
 end
